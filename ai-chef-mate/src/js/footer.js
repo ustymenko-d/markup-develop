d07bd414459c1da7005a6d3875814de76b4b73e0
ustymenko-d <ustymenko.denys@gmail.com>
2025-06-29 15:31:09 +0300

    Footer LCP fix

--- ai-chef-mate/src/js/footer.js
+++ ai-chef-mate/src/js/footer.js
@@ -1,8 +1,12 @@
+const footer = document.querySelector('#footer')
+const offset = document.querySelector('#offset')
+
 const offsetSize = () => {
-	document.querySelector('#offset').style.paddingBottom = `${
-		document.querySelector('#footer').clientHeight
-	}px`
+	offset.style.paddingBottom = `${footer.clientHeight}px`
 }
 
-window.addEventListener('load', offsetSize)
+window.addEventListener('load', () => {
+	footer.classList.toggle('hidden', false)
+	offsetSize()
+})
 window.addEventListener('resize', offsetSize)
